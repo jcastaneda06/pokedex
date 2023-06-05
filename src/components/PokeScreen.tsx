1f2908e318b26bfecc5149b1d69eded3b430a3b1
Jesus Castaneda <jcs.98126@gmail.com>
2023-06-05 10:01:50 -0500

    Implemented pokemon types

--- src/components/PokeScreen.tsx
+++ src/components/PokeScreen.tsx
@@ -1,20 +1,51 @@
 import { LoadingOutlined, ArrowLeftOutlined, ArrowRightOutlined } from "@ant-design/icons"
 import axios from "axios"
-import { useEffect, useState } from "react"
+import { use, useEffect, useState } from "react"
 import { Pokemon } from "~/models/pokemon"
 
 interface Props {
     pokemon: Pokemon | null | undefined;
-    pokemonName: string;
+    pokemonNameInput: string;
     searching: boolean;
 }
 
 
-const PokeScreen = ({ pokemon, pokemonName, searching }: Props) => {
+const PokeScreen = ({ pokemon, pokemonNameInput, searching }: Props) => {
+    const [pokemonName, setPokemonName] = useState('')
 
+    const types = [
+        "normal",
+        "fire",
+        "water",
+        "grass",
+        "electric",
+        "ice",
+        "fighting",
+        "poison",
+        "ground",
+        "flying",
+        "psychic",
+        "bug",
+        "rock",
+        "ghost",
+        "dark",
+        "dragon",
+        "steel",
+        "fairy",
+        "unknown",
+        "shadow"
+    ]
+
+    function handlePokemonNameInput() {
+        setPokemonName(pokemonName)
+    }
+
+    useEffect(() => {
+        setPokemonName(pokemonNameInput)
+    }, [])
+    
     return (
         <div className="flex flex-col">
-            <input type="text" value={pokemonName} placeholder="Who's that pokemon?" />
             <div className="bg-white my-4 p-2 rounded">
                 <div className="flex bg-green-600 text-green-900 font-bold p-2 min-h-[110px] rounded">
                     {pokemon && !searching ? (
@@ -25,11 +56,16 @@ const PokeScreen = ({ pokemon, pokemonName, searching }: Props) => {
                                 <p>Height: {pokemon.height / 10}m</p>
                                 <p>Weight: {pokemon.weight / 10}kg</p>
                             </div>
-                            <div className="flex-1 flex justify-center items-center">
+                            <div className="flex-1 flex flex-col">
                                 <img src={pokemon.sprites.front_default
                                     ? pokemon.sprites.front_default
                                     : pokemon.sprites.front_default_female
                                 } alt="" />
+                                <div className="flex gap-2">
+                                    {pokemon.types.map((type, index) => (
+                                        <p className={types.find(t => t === type.type.name) + ' text-sm text-center flex-1'} key={index}>{type.type.name}</p>
+                                    ))}
+                                </div>
                             </div>
                         </>
                     ) : (

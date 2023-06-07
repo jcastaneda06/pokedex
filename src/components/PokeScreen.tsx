b07135a8918cd559daa8c19553bd5aa19aff16c6
Jesus Castaneda <jcs.98126@gmail.com>
2023-06-07 04:24:35 -0500

    improved readability

--- src/components/PokeScreen.tsx
+++ src/components/PokeScreen.tsx
@@ -16,10 +16,7 @@ const PokeScreen = ({ pokemon, pokemonNameInput, searching }: Props) => {
     const [weakTo, setWeakTo] = useState<DamageRelations>({
         double_damage_from: [],
         double_damage_to: [],
-
     })
-
-    let test: number = 1
 
     const types = [
         "normal",
@@ -76,51 +73,55 @@ const PokeScreen = ({ pokemon, pokemonNameInput, searching }: Props) => {
         <div className="flex flex-col">
             <div className="bg-white my-4 p-2 rounded">
                 <div className="flex gap-4 bg-green-600 text-green-900 font-bold text-sm p-2 min-h-[110px] rounded">
-                    {pokemon && !searching ? (
-                        <>
-                            <div className="flex-1 flex flex-col">
-                                <p>Pokedex: {pokemon.order}</p>
-                                <p>Name: {pokemon.name}</p>
-                                <p>Height: {pokemon.height / 10}m</p>
-                                <p>Weight: {pokemon.weight / 10}kg</p>
-                                <div className="flex flex-col mt-auto">
-                                    <p>Weak to:</p>
-                                    <div className="flex flex-row flex-wrap gap-2">
-                                        {types.map((t, index) => (
-                                            <>
-                                                {weakTo.double_damage_from.find(w => w.name === t)
-                                                    ? (<p className={t + ' text-center flex-1'} key={index}>{t}</p>)
-                                                    : (<></>)
-                                                }
-                                            </>
+                    {pokemon && !searching
+                        ? (
+                            <>
+                                <div className="flex-1 flex flex-col">
+                                    <p>Pokedex: {pokemon.order}</p>
+                                    <p>Name: {pokemon.name}</p>
+                                    <p>Height: {pokemon.height / 10}m</p>
+                                    <p>Weight: {pokemon.weight / 10}kg</p>
+                                    <div className="flex flex-col mt-auto">
+                                        <p>Weak to:</p>
+                                        <div className="flex flex-row flex-wrap gap-2">
+                                            {types.map((type, index) => (
+                                                <>
+                                                    {
+                                                        weakTo.double_damage_from.find(w => w.name === type)
+                                                        ? (<p className={type + ' text-center flex-1'} key={index}>{type}</p>)
+                                                        : (<></>)
+                                                    }
+                                                </>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex-1 flex flex-col">
+                                    <img src={
+                                        pokemon.sprites.front_default
+                                            ? pokemon.sprites.front_default
+                                            : pokemon.sprites.front_default_female}
+                                        alt="" />
+                                    <div className="flex gap-2">
+                                        {pokemon.types.map((type, index) => (
+                                            <p className={types.find(t => t === type.type.name) + ' text-sm text-center flex-1'} key={index}>{type.type.name}</p>
                                         ))}
                                     </div>
                                 </div>
-                            </div>
-                            <div className="flex-1 flex flex-col">
-                                <img src={pokemon.sprites.front_default
-                                    ? pokemon.sprites.front_default
-                                    : pokemon.sprites.front_default_female
-                                } alt="" />
-                                <div className="flex gap-2">
-                                    {pokemon.types.map((type, index) => (
-                                        <p className={types.find(t => t === type.type.name) + ' text-sm text-center flex-1'} key={index}>{type.type.name}</p>
-                                    ))}
-                                </div>
-                            </div>
-                        </>
-                    ) : (
-                        <>{searching ? (
-                            <div className="flex-1 flex justify-center items-center text-2xl">
-                                <LoadingOutlined />
-                            </div>
-                        ) : (
-                            <div className="flex-1 flex text-2xl justify-center items-center text-center">
-                                404 Not Found!
-                            </div>
-                        )}</>
-                    )}
-
+                            </>)
+                        : (
+                            <>
+                                {searching ? (
+                                    <div className="flex-1 flex justify-center items-center text-2xl">
+                                        <LoadingOutlined />
+                                    </div>
+                                ) : (
+                                    <div className="flex-1 flex text-2xl justify-center items-center text-center">
+                                        404 Not Found!
+                                    </div>
+                                )}
+                            </>
+                        )}
                 </div>
             </div>
         </div>
